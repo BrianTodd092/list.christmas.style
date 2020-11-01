3e149157cb33af9aba822593f7051074e6412880
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-11-01 16:15:27 +0000

    Add files via upload
    Christmas style List Dec 1 to Jan 6.

---
+++ script.js
@@ -0,0 +1,30 @@
+var date = new Date();
+var d = date.getMonth();
+
+
+if(d == 11){
+var x = document.getElementsByTagName('ul')[0];
+
+
+x.className= 'b';
+
+
+}
+
+
+else if ( d == 0 && date.getDate() < 7 ) {
+
+var x = document.getElementsByTagName('ul')[0];
+
+
+x.className= 'b';
+
+
+
+}
+
+
+
+ 
+
+
